892680bce5060ff9f9d092ae2eaf574994282e44
TatsianaYurchik <t26yurchik@gmail.com>
2022-11-29 16:50:27 +0300

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Container } from "react-bootstrap";
+
+
+const Header = () => {
+
+    return (
+        <Container>
+
+{/* <Row>
+        <Col><div className="line"></div></Col>
+        <Col><h1>LILLY COLLECTION</h1></Col>
+        <Col> <div className="line"></div></Col>
+      </Row> */}
+       <p className="line"><span>LILLY COLLECTION</span></p>
+            
+            
+           
+        </Container>
+
+    );
+}
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import { Button } from 'react-bootstrap';
+import { Container } from "react-bootstrap";
+
+function Home () {
+  return (
+    <Container>
+     <Header/>
+     
+     </Container>
+  );
+}
+
+export default Home;
