aba449d9c8e9aa12a77c8cbfcc0249fd7495fe28
TatsianaYurchik <t26yurchik@gmail.com>
2022-11-29 23:09:39 +0300

    add home page info

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,15 +8,39 @@ const Header = () => {
     return (
         <Container>
 
-{/* <Row>
-        <Col><div className="line"></div></Col>
+            {/* <Row>
+        <Col><div className="line"><hr></hr></div></Col>
         <Col><h1>LILLY COLLECTION</h1></Col>
         <Col> <div className="line"></div></Col>
       </Row> */}
-       <p className="line"><span>LILLY COLLECTION</span></p>
+            <p className="line"><span>LILLY COLLECTION</span></p>
+
+            <Row>
+        <Col className="shipping">FREE SHIPPING <span className="material-symbols-outlined align-middle mb-1">
+          account_circle
+        </span></Col>
+        <Col className="buying">
+         
             
+              <span className="material-symbols-outlined mt-1 account">
+                account_circle
+              </span> 
+             <span className="align-text-bottom logIn"> Log in</span>
             
-           
+              <span className="material-symbols-rounded trolley">
+                shopping_cart
+              </span>
+          
+        </Col>
+      </Row>
+      <Row className='menu'>
+        <Col>HOME</Col>
+        <Col>SHOP</Col>
+        <Col>LOOKBOOK</Col>
+        <Col>WHERE TO BUY</Col>
+        <Col>VISIT US</Col>
+      </Row>
+
         </Container>
 
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,23 @@
 import Header from "../components/Header";
 import { Button } from 'react-bootstrap';
 import { Container } from "react-bootstrap";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Image } from "react-bootstrap";
+import Cycle from "./cycle.webp";
 
-function Home () {
+
+function Home() {
   return (
-    <Container>
-     <Header/>
-     
-     </Container>
+    <Container className="mt-3 px-5">
+
+      <Header />
+      {/* <div></div> */}
+    
+    {/* <img src="cycle.webp"> </img> */}
+    
+    <Image img alt="Summer collection" src={Cycle} className="main_image"/>
+    </Container>
   );
 }
 
