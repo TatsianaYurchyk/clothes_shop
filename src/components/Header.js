29cc2d58d87609e8e2db24cddabe9d654a396428
TatsianaYurchik <t26yurchik@gmail.com>
2022-11-30 22:23:29 +0300

    add info to home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,17 +8,10 @@ const Header = () => {
     return (
         <Container>
 
-            {/* <Row>
-        <Col><div className="line"><hr></hr></div></Col>
-        <Col><h1>LILLY COLLECTION</h1></Col>
-        <Col> <div className="line"></div></Col>
-      </Row> */}
             <p className="line"><span>LILLY COLLECTION</span></p>
 
             <Row>
-        <Col className="shipping">FREE SHIPPING <span className="material-symbols-outlined align-middle mb-1">
-          account_circle
-        </span></Col>
+        <Col className="shipping">FREE SHIPPING </Col>
         <Col className="buying">
          
             
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,16 @@ import { Container } from "react-bootstrap";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Image } from "react-bootstrap";
-import Cycle from "./cycle.webp";
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+
+import Main from "./cycle.webp";
+import Pic1 from "./pic1.webp";
+import Pic2 from "./pic2.webp";
+import Pic3 from "./pic3.webp";
+import Pic4 from "./pic4.webp";
+import Pic5 from "./pic5.webp";
+import Pic6 from "./pic6.webp";
 
 
 function Home() {
@@ -12,11 +21,17 @@ function Home() {
     <Container className="mt-3 px-5">
 
       <Header />
-      {/* <div></div> */}
-    
-    {/* <img src="cycle.webp"> </img> */}
-    
-    <Image img alt="Summer collection" src={Cycle} className="main_image"/>
+      <Image img alt="Summer collection" src={Main} className="main_image" />
+      <div className="round"> <div className="adv_text"><h2>SUMMER COLLECTION</h2> <p className="adv_text2">Now in stores <br />To get the look come <br /><a className="link">visit us </a></p></div></div>
+      <p className="line"><span>summer 2023</span></p>
+
+      <div class="wrapper_pic">
+        <div> <Image img alt="relax style" src={Pic1} className="pic" /></div>
+        <div><Image img alt="holiday style" src={Pic2} className="pic" /></div>
+        <div><Image img alt="casual style" src={Pic3} className="pic" /></div>
+        <div><Image img alt="date style" src={Pic4} className="pic" /></div>
+
+      </div>
     </Container>
   );
 }
